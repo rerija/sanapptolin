d86b30e34fcb8a9862b126073c0e91d2ef20b4eb
DESKTOP-B438I9E\user <rerijaapps@gmail.com>
2016-11-08 18:48:40 +0100

    - Creacion del proyecto base.

---
+++ SanApptolin/app/src/main/java/com/rerijaapps/sanapptolin/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rerijaapps.sanapptolin.Activities;
+
+import org.androidannotations.annotations.EActivity;
+
+import com.rerijaapps.sanapptolin.R;
+
+import android.support.v7.app.AppCompatActivity;
+
+@EActivity ( R.layout.activity_main )
+public class MainActivity extends AppCompatActivity
+{
+
+
+
+}
